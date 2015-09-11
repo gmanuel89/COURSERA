0886dd54ba7b9ac79a7aa40941c552e1882907a3
Manuel <gmanuel89+github@gmail.com>
2015-09-11 19:06:51 +0200

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -12,7 +12,7 @@ data <- csvFiles[[1]]
 # For each csvFile (after the first):
 for (f in 2:length(csvFiles)) {
 	# Merge dataframes by rows
-	data <- merge (data, csvFiles[[f]], all=TRUE)
+	data <- merge (data, csvFiles[[f]], all=TRUE, sort=FALSE)
 }
 ############# Return the mean only if the pollutant inserted is actually in the dataset
 if (pollutant %in% names(data)) {
