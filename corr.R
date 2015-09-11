b48a7a0b59c4ef214fb90b54b2dcc581e3ed9618
Manuel <gmanuel89+github@gmail.com>
2015-09-11 19:06:34 +0200

    Update corr.R

--- corr.R
+++ corr.R
@@ -12,22 +12,25 @@ data <- csvFiles[[1]]
 # For each csvFile (after the first):
 for (f in 2:length(csvFiles)) {
 	# Merge dataframes by rows
-	data <- merge (data, csvFiles[[f]], all=TRUE)
+	data <- merge (data, csvFiles[[f]], all=TRUE, sort=FALSE)
 }
 # Generate the result Dataframe
 correlationVector <- numeric()
 # For every ID
-for (i in data$ID) {
+for (i in unique(data$ID)) {
 	# Select the rows which ID is equal to i
-	idSubset <- data [data$ID %in% i,]
+	idSubset <- data [data$ID == i,]
 	# Compute the number of complete cases
 	completeCases <- complete.cases(idSubset)
 	ccNumber <- length(completeCases [completeCases == TRUE])
 	# Caculate the correlation only if the number of complete cases is above the threshold
-	if (ccNumber >= threshold) {
-		corPollutants <- cor (idSubset$sulfate, idSubset$nitrate)
+	if (ccNumber >= threshold && ccNumber !=0) {
+		# Isolate the complete cases only
+		idSubsetCC <- idSubset [which(completeCases ==TRUE),]
+		# Compute the correlation
+		corPollutants <- cor (idSubsetCC$sulfate, idSubsetCC$nitrate)
 		# Fill in the result vector
-		correlationVector <- append (correlationVector, corPollutants)
+		correlationVector <- append (correlationVector, round(corPollutants,5))
 	}
 }
 return (correlationVector)
